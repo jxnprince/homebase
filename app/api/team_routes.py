84c39db26d8ec98a70e2150b80d931f6c9d3b5b3
Ashley Brown <ashleybrown101789@gmail.com>
2021-04-27 14:50:11 -0700

    working on routes/form

--- app/api/team_routes.py
+++ app/api/team_routes.py
@@ -11,8 +11,42 @@
 # DELETE route
 # Alert window check confirms before deleting
     #for each project
-        # all associated tasks.projectId 
+        # all associated tasks.projectId
         # messageboard comments.projectId
         # Deletes selected Project
     #Deletes all mentions of selected team in usersOnTeam.teamId
     #Deletes Selected team
+
+from flask import Blueprint, session
+from app.models import Team, User
+from app.forms import TeamForm, SearchForm
+
+team_routes = Blueprint('teams', __name__)
+
+
+@team_routes.route('/search', methods=['GET', 'POST'])
+def search():
+    searchForm = SearchForm()
+    users = Users.query
+
+
+@team_routes.route('homebase/users/<int:id>/teams')
+def teams(id):
+    user = User.query.get(id)
+    teams = user.teams
+    return {"teams": [team.to_dict() for team in teams]}
+
+
+@team_routes.route('homebase/users/<int:id>/teams', methods=['POST'])
+def teams_post():
+    form = TeamForm()
+    form['csrf_token'].data = request.cookies['csrf_token']
+    if form.validate_on_submit():
+        team = Team(
+            teamName=form.data['Team Name'],
+            users=
+            projects=[]
+        )
+        db.session.add(team)
+        db.session.commit()
+        return team.to_dict()
