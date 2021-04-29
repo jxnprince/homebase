1c548ba0434475e8d3d83c8fb37dd180166b60d1
LaTCheatam <obsidyenmoon@gmail.com>
2021-04-29 07:16:07 -0700

    task routes almost complete

--- app/api/task_routes.py
+++ app/api/task_routes.py
@@ -1,6 +1,6 @@
 from flask import Blueprint, jsonify, request, redirect, url_for
 from flask_login import current_user, login_required
-from ..forms.create_task import CreateTaskForm
+from ..forms.task_form import CreateTaskForm
 from ..models.project import Project
 from ..models.team import Team
 from ..models.usersOnTeam import UsersOnTeams
@@ -10,50 +10,73 @@ from ..models.comment import Comment
 from ..models.db import db
 
 
-task_routes = Blueprint("tasks", __name__ url_prefix="/users/:userId/teams/:teamId/projects/:projectId")
+task_routes = Blueprint("tasks", __name__)
 
 # "homebase/users/:id/teams/:id/projects/:id/tasks/create"
 # POST - appears on project page in form modal
-@task_routes("/tasks/create", methods=["POST"])
-@login.required
+@task_routes.route("create/task/<int:taskId>", methods=["POST"])
+# @login.required
 def create_task(teamId):
-    team = Team.query.get(teamId)
     form = CreateTaskForm()
+    team = Team.query.get(teamId)
+    form['csrf_token'].data = request.cookies['csrf_token']
     form.user.choices = [(user.id, user.firstname, user.lastname) for user in team.users]
     if form.validate_on_submit():
-        task = Task(
-            taskName=form.taskName.data,
-            taskBody=form.taskBody.data,
-            dueDate=form.dueDate.data,
-            completed=form.completed.data,
-            assignedUserId=form.assignedUserId.data,
-            projectId=form.projectId.data
-        )
+        data = Task()
+        form.populate_obj(data)
+        data.userId = userId
         db.session.add(task)
-    db.session.commit()
-    return redirect(url_for('/tasks/:taskId'))
+        db.session.commit()
+        return data.to_dict()
+    else:
+        return 'Something has gone wrong. Task not created. '
 
 # "homebase/users/:id/teams/:id/projects/:id/tasks/:id"
 # GET 
 # Render all information about a singular task as well as host edit and delete buttons
-@task_routes("/tasks/create", methods=["GET"])
-@login.required
+@task_routes.route("/tasks/<int:taskId>", methods=["GET"])
+# @login.required
+
 
 # "homebase/users/:id/teams/:id/projects/:id/tasks/"
 # GET 
 # Render ten tasks organized by due date decending** (DUE SOONEST FIRST)
-@task_routes("/tasks/create", methods=["GET"])
-@login.required
+@task_routes.route("/tasks/<int:dueDate>", methods=["GET"])
+# @login.required
+
 
 # "homebase/users/:id/teams/:id/projects/:id/tasks/:id"
 # Patch 
 # renders a modal with a form for creating a task with prepopulated forms.
-@task_routes("/tasks/create", methods=["PATCH"])
-@login.required
+@task_routes.route("<int:id>/edit", methods=["PATCH"])
+# @login.required
+def edit_task(id):
+    task = Task.query.get(id)
+    userId = task.userId
+    db.session.delete(task)
+    db.session.commit()
+    taskName = request.form['taskTitle']
+    taskBody = request.form['taskDescription']
+    dueDate = request.form['dueDate']
+    completed = request.form['completed']
+    assignedUserId = request.form['assignedUserId']
+    task = Task(taskName=taskName, taskBody=taskBody, dueDate=dueDate,taskId=taskId)
+    db.session.add(task)
+    db.session.commit()
+    return f'{Task} was successfully updated'
+else:
+    return 'Edit failed, please try again.'
 
 # "homebase/users/:id/teams/:id/projects/:id/tasks/:id"
 # Delete 
     # Set tasks.assignedUserId to Null
     # Delete's specified task
-@task_routes("/tasks/create", methods=["DELETE"])
-@login.required
+@task_routes.route("<int:id>/delete", methods=['DELETE'])
+# @login.required
+def delete_task(id):
+    task = Task.query.get(id)
+    db.session.delete(task)
+    db.session.commit()
+    return f'Task: {task.taskName} was Deleted'
+else:
+    return f'Task: {task.taskName} was Not Deleted.'
