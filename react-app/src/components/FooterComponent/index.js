e4b61a895efb239d44d2ce04099d8675f66151fa
jxnprince <68714241+jxnprince@users.noreply.github.com>
2021-06-10 14:22:27 -0600

    More last-minute styling

--- react-app/src/components/FooterComponent/index.js
+++ react-app/src/components/FooterComponent/index.js
@@ -40,22 +40,22 @@ function FooterComponent() {
         drop={"up"}
         variant="secondary"
         size="sm"
-        title={'Maricio Harris'}>
-            <Dropdown.Item href={'https://www.linkedin.com/in/maricio-harris-0547491b9/'}>LinkedIn</Dropdown.Item>
-            <Dropdown.Item href={'https://github.com/maricio41'}>GitHub</Dropdown.Item>
+        title={'Jackson Prince'}>
+            <Dropdown.Item href={'https://www.linkedin.com/in/jackson-prince-4b3a637b/'}>LinkedIn</Dropdown.Item>
+            <Dropdown.Item href={'https://github.com/jxnprince'}>GitHub</Dropdown.Item>
         </DropdownButton>
     </div>
 
-    <div className="footer-button">
+        <div className="footer-button">
         <DropdownButton
         key={"up"}
         id="dropdown-button-drop-up"
         drop={"up"}
         variant="secondary"
         size="sm"
-        title={'Jackson Prince'}>
-            <Dropdown.Item href={'https://www.linkedin.com/in/jackson-prince-4b3a637b/'}>LinkedIn</Dropdown.Item>
-            <Dropdown.Item href={'https://github.com/jxnprince'}>GitHub</Dropdown.Item>
+        title={'Maricio Harris'}>
+            <Dropdown.Item href={'https://www.linkedin.com/in/maricio-harris-0547491b9/'}>LinkedIn</Dropdown.Item>
+            <Dropdown.Item href={'https://github.com/maricio41'}>GitHub</Dropdown.Item>
         </DropdownButton>
     </div>
 
